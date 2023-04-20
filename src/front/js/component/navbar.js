e4614d34c6063330e93ba8f21b53bd6f789ceb46
Celeste Bareiro <cnyx7@proton.me>
2023-04-20 02:17:20 +0000

    navbar y footer

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoMartin from "../../images/Logo-martin.png";
 
 export const Navbar = () => {
+	function myFunction() {
+		var x = document.getElementById("myLinks");
+		if (x.style.display === "block") {
+			x.style.display = "none";
+		} else {
+			x.style.display = "block";
+		}
+	}
+
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<>
+			<div className="nav">
+				<div className="container">
+					<div className="logo">
+						<img src={logoMartin} alt="Logo" style={{ width: "100px" }} />
+						<h1>MVMNT</h1>
+					</div>
+					<div className="list" id="myLinks">
+						<ul>
+							<li>
+								<a href="#">Inicio</a>
+							</li>
+							<li>
+								<a href="#">Nosotros</a>
+							</li>
+							<li>
+								<a href="#">Planes</a>
+							</li>
+							<li>
+								<a href="#">Testimonios</a>
+							</li>
+							<li>
+								<a href="#">Equipo</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<button type="button">Iniciar Sesión</button>
+					</div>
+					<a
+						href="javascript:void(0);"
+						className="humb-menu"
+						onClick={myFunction}
+					>
+						<i className="fa fa-bars"></i>
+					</a>
 				</div>
 			</div>
-		</nav>
+		</>
 	);
 };
