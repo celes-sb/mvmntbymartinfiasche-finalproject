19d2466b1e45541d2c4002c374afb5b35e0fad76
Celeste Bareiro <cnyx7@proton.me>
2023-04-23 03:51:17 +0000

    final commit for the day rough draft of app layout with its components... work has just started

--- src/front/js/pages/contact.jsx
+++ src/front/js/pages/contact.jsx
@@ -10,59 +10,55 @@ export const Contact = () => {
                 <div className="container">
                     <div className="contact-us-content">
                         <div className="left-content">
-                            <h2>Contáctanos</h2>
+                            <h2>Contacto</h2>
                             <div className="form">
                                 <div className="row">
                                     <div className="row-c">
-                                        <label for="fname">Nombre</label>
+                                        <label for="fname">Nombre<span className="text-danger"> *</span></label>
                                         <input
+                                            className="input-sec"
                                             type="text"
                                             id="fname"
                                             name="fname"
-                                            placeholder="Enter your first name"
+                                            placeholder="Ingresá tu nombre"
                                         />
                                     </div>
                                     <div className="row-c">
-                                        <label for="lname">Apellido</label>
+                                        <label for="lname">Apellido<span className="text-danger"> *</span></label>
                                         <input
+                                            className="input-sec"
                                             type="text"
                                             id="lname"
                                             name="lname"
-                                            placeholder="Enter your last name"
+                                            placeholder="Ingresá tu apellido"
                                         />
                                     </div>
                                 </div>
 
                                 <label className="color" for="email">
-                                    Correo Electrónico
-                                </label>
+                                    Correo Electrónico<span className="text-danger"> *</span></label>
                                 <input
                                     className="input-sec"
                                     type="email"
                                     id="email"
                                     name="email"
-                                    placeholder="Enter your email"
+                                    placeholder="Ingresá tu correo electrónico"
                                 />
 
                                 <label className="color" for="message">
-                                    Mensaje
-                                </label>
+                                    Comentario o consulta<span className="text-danger"> *</span></label>
                                 <textarea
+                                    className="input-sec"
                                     name="msg"
                                     id="message"
-                                    placeholder="Enter your message"
+                                    placeholder="Escribí tus consultas o comentarios"
                                 ></textarea>
 
-                                <button className="btn"> ENVIAR</button>
+                                <button className="btn">ENVIAR</button>
                             </div>
                         </div>
                         <div className="right-content">
-                            <img className="img-1" src={fotoGimnasio} alt="" />
-                            <img className="img-2" src={dotOrnament} alt="" />
-                            <div className="text">
-                                <img src={mapIcon} alt="" />
-                                <p>Av. Belgrano 342, Buenos Aires, Argentina</p>
-                            </div>
+                            <img className="img-1" alt="Contact Form Image CambiarPorLoQueDeMartin" style={{ width: "400px", height: "500px" }} src="https://c0.wallpaperflare.com/preview/821/40/875/levitation-beauty-smoke-circus.jpg" />
                         </div>
                     </div>
                 </div>
