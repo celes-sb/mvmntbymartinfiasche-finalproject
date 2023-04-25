e7da35f5d5c719f39befb9f76cf19b3281fe0974
Sebastián Bonilla <sebastian.bonillad@gmail.com>
2023-04-24 22:21:56 -0500

    ok

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -44,6 +44,7 @@ const Layout = () => {
             <Route element={<Contact />} path="/escribime" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
+            <Route element={<Login />} path="/signup" />
             <Route element={<Login />} path="/login" />
           </Routes>
           <Footer />
