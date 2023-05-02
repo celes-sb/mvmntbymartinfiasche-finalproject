fdf9bcf0d2284ffce254871997358ad1c3c343ee
Celeste <cnyx7@proton.me>
2023-05-01 23:22:52 -0300

    last commit for the day

--- src/front/js/pages/contact.jsx
+++ src/front/js/pages/contact.jsx
@@ -1,78 +1,79 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 //import { Context } from "../store/appContext";
 
 export const Contact = () => {
   const contactoManos =
     "http://drive.google.com/uc?export=view&id=1fYc_uMZtdSOR-i7E0ckCs6IB4pz-i3xY";
 
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [comments, setComments] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can add your code to handle form submission
+  };
+
   https: return (
     <>
       <section
-        className="container-fluid contact-us ps-0 pt-5 pb-5 mb-5mt-5 border border-warning rounded"
+        className="customer-says border border-warning rounded"
         style={{
           backgroundImage: `url(${contactoManos})`,
           backgroundSize: "cover",
         }}
-        alt="Manos haciendo contacto, blanco y negro"
       >
-        <div className="container">
-          <div className="contact-us-content">
-            <div className="left-content">
-              <h2>Hablemos</h2>
-              <h4>¡Escribime y sumate al movimiento!</h4>
-              <div className="form">
-                <div className="row">
-                  <div className="row-c">
-                    <label for="fname">
-                      Nombre<span className="text-danger"> *</span>
-                    </label>
-                    <input
-                      className="input-sec"
-                      type="text"
-                      id="fname"
-                      name="fname"
-                      placeholder="Ingresá tu nombre"
-                    />
-                  </div>
-                  <div className="row-c">
-                    <label for="lname">
-                      Apellido<span className="text-danger"> *</span>
-                    </label>
-                    <input
-                      className="input-sec"
-                      type="text"
-                      id="lname"
-                      name="lname"
-                      placeholder="Ingresá tu apellido"
-                    />
-                  </div>
-                </div>
-
-                <label className="row-c" for="email">
-                  Correo Electrónico<span className="text-danger"> *</span>
-                </label>
+        <div className="container customer-says-content bg-white bg-opacity-75 rounded p-5">
+          <div className="top">
+            <h2>Escribime y sumate al movimiento</h2>
+          </div>
+          <div className="bottom">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="name">Nombre</label>
                 <input
-                  className="input-sec"
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Ingresá tu correo electrónico"
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
                 />
-
-                <label className="color" for="message">
-                  Comentario o consulta
-                  <span className="text-danger"> *</span>
-                </label>
-                <textarea
-                  className="input-sec"
-                  name="msg"
-                  id="message"
-                  placeholder="Escribí tus consultas o comentarios"
-                ></textarea>
-
-                <button className="btn">ENVIAR</button>
               </div>
-            </div>
+              <div className="form-group">
+                <label htmlFor="lastName">Apellido</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  value={lastName}
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Correo electrónico</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="comments">Comentarios o consultas</label>
+                <textarea
+                  className="form-control"
+                  id="comments"
+                  rows="3"
+                  value={comments}
+                  onChange={(event) => setComments(event.target.value)}
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                ¡Hablemos!
+              </button>
+            </form>
           </div>
         </div>
       </section>
