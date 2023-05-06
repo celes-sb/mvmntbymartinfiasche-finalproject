4d01790d1b2b515d51b09039376ac70e88fadfd4
Celeste <cnyx7@proton.me>
2023-05-05 23:23:48 -0300

    changes

--- docs/assets/reset_migrations_local_postgre.bash
+++ docs/assets/reset_migrations_local_postgre.bash
@@ -4,4 +4,4 @@ psql -U soledadceleste -c 'DROP DATABASE celestesoledad;' || true &&
 psql -U soledadceleste -c 'CREATE DATABASE celestesoledad;' &&
 psql -U soledadceleste -c 'CREATE EXTENSION unaccent;' -d celestesoledad &&
 pipenv run migrate &&
-pipenv run upgrade[]
+pipenv run upgrade
